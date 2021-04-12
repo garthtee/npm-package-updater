2ed21a89e7e58cdaa26bfa451266722bbe16f15f
Garth Toland <tolandgarth@gmail.com>
2021-04-12 22:58:17 +0100

    Error handling for package search

--- src/utils/dependencyUtils.ts
+++ src/utils/dependencyUtils.ts
@@ -1,5 +1,6 @@
 import {getPackage} from "../api/api";
 import {promises as fs} from "fs";
+import { AxiosError } from "axios";
 
 const getDependencies = async (packageFilePath: string) => {
   const fileContents = await fs.readFile(packageFilePath);
@@ -21,32 +22,39 @@ const getLatestVersion = async (dep: string, currentVersion: string) => {
     return currentVersion;   
   }
 
-  const packageInfo = await getPackage(dep);
-  const numberRegex = /[^\d.]/g;
+  try {
+    const packageInfo = await getPackage(dep);
 
-  const latestVersion = packageInfo["dist-tags"]?.latest;
-  const currentSplit = currentVersion?.replaceAll(numberRegex, "").split(".");
-  const latestSplit = latestVersion?.replaceAll(numberRegex, "").split(".");
+    const numberRegex = /[^\d.]/g;
 
-  const latestMajor = latestSplit[0];
-  const latestMinor = latestSplit[1];
-  const latestPatch = latestSplit[2];
+    const latestVersion = packageInfo["dist-tags"]?.latest;
+    const currentSplit = currentVersion?.replaceAll(numberRegex, "").split(".");
+    const latestSplit = latestVersion?.replaceAll(numberRegex, "").split(".");
 
-  const currentMajor = currentSplit[0];
-  const currentMinor = currentSplit[1];
-  const currentPatch = currentSplit[2];
+    const latestMajor = latestSplit[0];
+    const latestMinor = latestSplit[1];
+    const latestPatch = latestSplit[2];
 
-  if (
-    latestMajor > currentMajor ||
-    (latestMinor > currentMinor && latestMajor === currentMajor) ||
-    (latestPatch > currentPatch &&
-      latestMinor === currentMinor &&
-      latestMajor === currentMajor)
-  ) {
-    if (!currentVersion[0].match(/^\d/)) {
-      return `${currentVersion[0]}${latestVersion}`
+    const currentMajor = currentSplit[0];
+    const currentMinor = currentSplit[1];
+    const currentPatch = currentSplit[2];
+
+    if (
+      latestMajor > currentMajor ||
+      (latestMinor > currentMinor && latestMajor === currentMajor) ||
+      (latestPatch > currentPatch &&
+        latestMinor === currentMinor &&
+        latestMajor === currentMajor)
+    ) {
+      if (!currentVersion[0].match(/^\d/)) {
+        return `${currentVersion[0]}${latestVersion}`
+      }
+      return latestVersion;
     }
-    return latestVersion;
+  } catch (error) {
+    if (error?.isAxiosError) {
+      console.error(error.message);
+    }
   }
 
   return currentVersion;
